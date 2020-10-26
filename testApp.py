85183b8689a1887a00a0315685a7b00229aadca6
Marcio Gomes <marciobivs@gmail.com>
2020-10-26 09:07:48 -0300

    File App using new file endpoint with format config

--- testApp.py
+++ testApp.py
@@ -12,20 +12,20 @@ if __name__ == "__main__":
 
     fparser = FileParser()
     
-    #fendpoint = FileEndpoint()
-    #fendpoint.config('saida.txt')
+    fendpoint = FileEndpoint()
+    fendpoint.config('saida.txt', 'json') # format = 'csv', 'tsv' or 'json'
     hendpoint = HTTPEndpoint()
-    hendpoint.config('http://127.0.0.1:8006/inputStream')
+    #hendpoint.config('http://127.0.0.1:8006/inputStream')
     #hendpoint.config('http://localhost:38080/logdata')
     
     fdevice.setParser(fparser)
-    #fdevice.addEndpoint(fendpoint)
-    fdevice.addEndpoint(hendpoint)
-    #fdevice.addFunction(function.Min(batchlen))
-    #fdevice.addFunction(function.Max(batchlen))
-    #fdevice.addFunction(function.Sum(batchlen))
-    #fdevice.addFunction(function.Count(batchlen))
-    #fdevice.addFunction(function.Mean(batchlen))
-    #fdevice.addFunction(function.StDev(batchlen))
+    fdevice.addEndpoint(fendpoint)
+    #fdevice.addEndpoint(hendpoint)
+    fdevice.addFunction(function.Min(batchlen))
+    fdevice.addFunction(function.Max(batchlen))
+    fdevice.addFunction(function.Sum(batchlen))
+    fdevice.addFunction(function.Count(batchlen))
+    fdevice.addFunction(function.Mean(batchlen))
+    fdevice.addFunction(function.StDev(batchlen))
 
     fdevice.run()
