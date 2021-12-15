ecba948f6687fbd727a8e734a4349c780bab8742
Marcio Gomes <marciobivs@gmail.com>
2021-12-15 20:51:09 -0300

    Added class SystemMonitoringInput

--- steam/input.py
+++ steam/input.py
@@ -1,4 +1,5 @@
 import os
+import psutil
 
 class Input:
     def readData(self):
@@ -21,3 +22,34 @@ class FileInput(Input):
                     if line[0] not in '# ': # skip empty and comments
                         return True, line
                         
+class SystemMonitoringInput(Input):
+    def __init__(self, interval_sec=1):
+        self._interval_sec = interval_sec
+
+    def readData(self):
+        cpu = psutil.cpu_percent(self._interval_sec)
+        mem = psutil.virtual_memory()
+        swap = psutil.swap_memory()
+        net = psutil.net_io_counters()
+        disk_usage = psutil.disk_usage('/')
+        disk_count = psutil.disk_io_counters()
+        
+        line = {
+            'cpu_pc': cpu,
+            'mem_pc': mem.percent,
+            'mem_used': mem.used,
+            'swap_pc': swap.percent,
+            'swap_used': swap.used,
+            'net_bytes_sent': net.bytes_sent,
+            'net_bytes_recv': net.bytes_recv,
+            'disk_pc': disk_usage.percent,
+            'disk_used':disk_usage.used,
+            'disk_read_count': disk_count.read_count,
+            'disk_write_count': disk_count.write_count,
+            'disk_read_bytes': disk_count.read_bytes,
+            'disk_write_bytes': disk_count.write_bytes,
+            'disk_read_time': disk_count.read_time,
+            'disk_write_time': disk_count.write_time
+        }
+
+        return True, line
